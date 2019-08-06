5ea8e23a5f8b0634958b2c7383649a153e2df7a7
William Best <wpbest@gmail.com>
2019-08-06 09:56:38 -0600

    Add page header

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
+  @Input() pageActive = '';
+  @Input() pageUrlActive = '';
+  
   constructor() { }
 
   ngOnInit() {
