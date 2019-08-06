4ab59032c62d8cb8d37b90a2eabb24ef65d40b9a
William Best <wpbest@gmail.com>
2019-08-06 15:00:08 -0600

    Added more components

--- src/app/blog-comment/blog-comment.component.ts
+++ src/app/blog-comment/blog-comment.component.ts
@@ -2,17 +2,81 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
 
+import { BlogService } from '../blog-service/blog.service';
+import { BlogReply } from '../blog-service/blog-reply.model';
+import { BlogPost } from '../blog-service/blog-post.model';
+import { AuthUserService } from '../auth/auth-user.service';
 
 @Component({
   selector: 'app-blog-comment',
   templateUrl: './blog-comment.component.html',
   styleUrls: ['./blog-comment.component.scss']
 })
-export class BlogCommentComponent implements OnInit {
+export class BlogCommentComponent implements OnInit, OnDestroy {
+  isComment = false;
+  @Input() commentDate;
+  @Input() comment;
+  @Input() id: number;
+  @Input() commentId: number;
+  @Input() displayCommentName: string;
+  replyForm: FormGroup;
+  postId: number;
+  replyComment: BlogReply[];
+  key: any;
+  dataSubscription: Subscription;
+  displayName = this.authUserService.diplayName;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private blogService: BlogService, private authUserService: AuthUserService) { }
 
   ngOnInit() {
+    this.postId = +this.route.snapshot.params['id'];
+    this.dataSubscription = this.blogService.getBlogData()
+      .subscribe(
+        (blogPost: BlogPost[]) => {
+          if (blogPost[this.postId - 1].blogComment[this.commentId].reply) {
+            this.replyComment = blogPost[this.postId - 1].blogComment[this.commentId].reply.reverse();
+          }
+        }
+      );
+    this.replyForm = new FormGroup({
+    'textareaReply': new FormControl(null)
+    })
+
   }
+
+  onReply(id: any) {
+    if (id === null) {
+      this.key = 0;
+    } else {
+      this.key = id;
+    }
+    this.isComment = !this.isComment;
+  }
+
+  onSubmit() {
+    const postId = this.postId - 1;
+    const textReply = this.replyForm.get('textareaReply').value;
+    const reply = new BlogReply(textReply, Date.now(), this.displayName, '');
+    this.blogService.addReply(postId, this.commentId, this.key, reply);
+    this.replyForm.reset();
+    this.isComment = false;
+    setTimeout(() => {
+      this.blogService.getBlogData()
+        .subscribe(
+          (data: Data) => {
+            this.replyComment = data[this.postId - 1].blogComment[this.commentId].reply.reverse()
+          }
+        )
+    }, 3000);
+  }
+
+  onCallSubmit() {
+    this.onSubmit();
+  }
+
+  ngOnDestroy() {
+    this.dataSubscription.unsubscribe();
+  }
+
 
 }
--- src/app/blog-post-item/blog-post-item.component.ts
+++ src/app/blog-post-item/blog-post-item.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Data, Params } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { BlogService } from '../blog-service/blog.service';
+import { BlogPost } from '../blog-service/blog-post.model';
+import { BlogComment } from '../blog-service/blog-comment.model';
+import { AuthUserService } from '../auth/auth-user.service';
 
 @Component({
   selector: 'app-blog-post-item',
   templateUrl: './blog-post-item.component.html',
-  styleUrls: ['./blog-post-item.component.scss']
+  styleUrls: ['./blog-post-item.component.css']
 })
-export class BlogPostItemComponent implements OnInit {
+export class BlogPostItemComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  blogPost: BlogPost;
+  paramsId: number;
+  resolveSubscription: Subscription;
+  commentForm: FormGroup;
+  blogComment: BlogComment;
+  key: any;
+  displayName = this.authUserService.diplayName;
+
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService,
+    private authUserService: AuthUserService
+  ) {}
 
   ngOnInit() {
+    this.paramsId = +this.route.snapshot.params['id'];
+    this.resolveSubscription = this.route.data
+      .subscribe(
+        (data: Data) => {
+          this.blogPost = data['blogPost'][this.paramsId - 1];
+          if (data['blogPost'][this.paramsId - 1].blogComment) {
+            this.blogComment = data['blogPost'][this.paramsId - 1].blogComment.reverse()
+          }
+        }
+      );
+    this.commentForm = new FormGroup({
+      'textareaComment': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    const commentLength = Object.keys(this.blogComment).length;
+    if (commentLength === null) {
+      this.key = 0
+    }else {
+      this.key = commentLength
+    }
+    const id = this.paramsId - 1;
+    const blogComment = this.commentForm.get('textareaComment').value;
+    const comment = new BlogComment(blogComment, Date.now(), [], this.displayName, '');
+    this.blogService.addComment(id, comment, this.key);
+    this.commentForm.reset();
+    setTimeout(() => {
+      this.blogService.getBlogData()
+        .subscribe(
+          (data: Data) => {
+            this.blogComment = data[this.paramsId - 1].blogComment.reverse()
+          }
+        )
+    }, 3000);
+  }
+
+  ngOnDestroy() {
+    this.resolveSubscription.unsubscribe();
   }
 
 }
