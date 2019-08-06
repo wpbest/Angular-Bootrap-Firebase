e46725b05f8163dbad0f23b071a2b2b578194a36
senshi111 <bane.nicovic@gmail.com>
2019-08-06 17:51:43 +0200

    fix carousel images

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  images = '../../assets/images/carousel/mb1.jpeg';
+  images2 = '../../assets/images/carousel/mb2.jpeg';
+  images3 = '../../assets/images/carousel/mb3.jpg';
   constructor() { }
 
   ngOnInit() {
