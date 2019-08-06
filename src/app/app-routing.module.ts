9b29cfc5981bd5aefa39b87e7a4abb1d019b9fff
William Best <wpbest@gmail.com>
2019-08-06 10:49:47 -0600

    Added signup signin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
